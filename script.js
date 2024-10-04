fed8e1ba23f95264e7ca4cd775ac2ad81f46af23
OriginalRelic <flashattack20@gmail.com>
2024-10-04 13:48:53 +0800

    Added in the API Intergration

--- script.js
+++ script.js
@@ -51,7 +51,12 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "da2ab04o0c741f6eb0bet4525f37f63d";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -59,22 +64,26 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(respone.data);
-
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">☀️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">T${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
-        <div class="weather-forecast-temp"><strong>26°</strong></div>
-        <div class="weather-forecast-temp"> 15°</div>
+        <div class="weather-forecast-temp"><strong>${Math.round(
+          day.temperature.maximum
+        )}°</strong></div>
+        <div class="weather-forecast-temp">${Math.round(
+          day.temperature.minimum
+        )}°</div>
       </div>
      </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
